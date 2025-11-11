f42ca4b1797992fb8ea9c2ce7e99af5c6ca99fc9
boukendesho <100831251+boukendesho@users.noreply.github.com>
2025-11-11 15:39:26 +0900

    script: make warning messages only to stderr. #3

--- scripts/wrapper.sh
+++ scripts/wrapper.sh
@@ -3,18 +3,22 @@
 ACK_FILE="$SNAP_USER_COMMON/.snap_acked"
 
 if [ ! -f "$ACK_FILE" ]; then
-    echo ""
-    echo "Caution: You are using the Snap version of curl."
-    echo "Due to Snap's sandbox nature, this version has some limitations."
-    echo "For example, it may not be able to access hidden folders in your home directory"
-    echo "or other restricted areas of the os."
-    echo "This means you may encounter errors when using snap curl to download files."
-    echo "For those case, you might want to use the native curl package."
-    echo "For details, see: https://github.com/boukendesho/curl-snap/issues/1"
-    echo ""
-    echo "To stop seeing this message, run the following command:"
-    echo "curl.snap-acked"
-    echo ""
+    cat << EOL >&2
+
+    Caution: You are using the Snap version of curl.
+    Due to Snap's sandbox nature, this version has some limitations.
+    For example, it may not be able to access hidden folders in your home directory
+    or other restricted areas of the os.
+
+    Which means you may encounter errors when using snap curl to download and execute some script.
+    For those cases, you might want to use the native curl package.
+    For details, see: https://github.com/boukendesho/curl-snap/issues/1
+    
+    To stop seeing this message, run the following command:
+    curl.snap-acked
+
+EOL
+
 fi
 
 $SNAP/usr/local/bin/curl "$@"
